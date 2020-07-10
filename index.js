513fcc55e51034ff8ef4f974dd2fbeccf7aad1ae
mauro <mauroeportillo@gmail.com!>
2020-07-10 00:48:23 -0300

    change listen port

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get('/world/total', cors(), function (req, res) {
 });
 
   
-app.listen(3000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000!');
 });
